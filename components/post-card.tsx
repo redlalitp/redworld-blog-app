43f9390be5581141a6254b87833a621477c91b78
Lalit Patil <lalitpatil@outlook.com>
2025-07-05 14:18:22 +0530

    chnaged image path

--- components/post-card.tsx
+++ components/post-card.tsx
@@ -2,7 +2,7 @@
 export const PostCard = ({post}) => {
     return(
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
-        <img className="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains"/>
+        <img className="w-full" src="images/pawn-king.jpg" alt="Sunset in the mountains"/>
         <div className="px-6 py-4">
             <div className="font-bold text-xl mb-2">{post.title}</div>
             <p className="text-gray-700 text-base truncate">
