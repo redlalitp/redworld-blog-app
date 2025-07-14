296303868353fb1327715e17f9c46325d9c8814e
Lalit Patil <lalitpatil@outlook.com>
2025-07-14 16:25:41 +0530

    background image change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,32 @@
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import {BackgroundProvider, useBackground} from "../lib/background-context";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+      <div className="min-h-screen max-w-4xl justify-self-center bg-black/30 backdrop-blur-sm text-gray-200">
+          <BackgroundProvider>
+              <LayoutWrapper>
+                <SessionProvider session={session}>
+                  <Component {...pageProps} />
+                </SessionProvider>
+              </LayoutWrapper>
+          </BackgroundProvider>
+      </div>
   );
+    function LayoutWrapper({ children }) {
+        const { background } = useBackground();
+
+        return (
+            <div
+                className="min-h-screen transition-all duration-500"
+                style={{
+                    backgroundImage: `url(/images/${background}.jpg)`,
+                }}
+            >
+                {children}
+            </div>
+        );
+    }
 }
